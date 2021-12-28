99aa207630d6098f8610056614afad6a98486c1d
tomermichaeli <48432327+tomermichaeli@users.noreply.github.com>
2021-12-28 17:26:24 +0200

    get data from mongodb

--- snapserver.js
+++ snapserver.js
@@ -1,5 +1,40 @@
 const express =  require("express");
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
+
+app.use(cors());
+
+const uri = "mongodb+srv://test:test@cluster0.anx9a.mongodb.net/thenewsil?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {useNewUrlParser: true}, {useUnifiedTopology: true});
+
+const DocSchema = {
+    headline: String,
+    body: String,
+    time: String
+}
+
+const Doc = mongoose.model("updates", DocSchema); //(collection, data schema)
+
+app.get("/", function(req, res)
+    {
+        Doc.find({}, function(err, updates){
+            res.json(updates)
+        }).sort({"_id": -1}).limit(4);
+    }
+);
+
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5000");
+    // MongoClient.connect(uri, {useNewUrlParser: true}, {useUnifiedTopology: true}, (error, result) => {
+    //     if (error) throw error;
+    //     database = result.db("thenewsil")
+    //     console.log("Connected to database ", database.toString())
+    // })
+}).on('error', function (err) {
+    console.log(err)
+});
