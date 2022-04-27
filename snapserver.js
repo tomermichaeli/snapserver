3141798f01a1c9eaa2a34c62cb653e54de68aca8
tomermichaeli <48432327+tomermichaeli@users.noreply.github.com>
2022-04-27 12:35:13 +0300

    minor

--- snapserver.js
+++ snapserver.js
@@ -2,8 +2,6 @@ const express =  require("express");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-
 app.use(cors());
 
 const bodyParser = require("body-parser");
